2b24bd5cdb1c18625ed5db34be90281facfec08e
mohamadreza <mohamadrezamohammady2019@gmail.com>
2023-03-30 10:44:45 -0700

    something changed

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,13 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
-    appBar: AppBar(title: Center(child: Text("I Am Rich"))),
+    backgroundColor: Colors.blueGrey[400],
+    appBar: AppBar(
+      backgroundColor: Colors.blueGrey[700],
+      title: Center(
+        child: Text("I Am Rich"),
+      ),
+    ),
     body: Center(
         child: Image(
       image: (AssetImage('images/diamond.jpg')),
